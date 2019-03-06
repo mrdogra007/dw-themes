2ae0d44f6ecb5813f7c1284b174f84d2620e5e61
Nishant Dogra <13938655+mrdogra007@users.noreply.github.com>
2019-03-06 17:06:28 +0530

    amessagetounbox data
    amessagetounbox data

---
+++ amessagetounbox/README.md
@@ -0,0 +1,7 @@
+# Wordpress theme refined for A Message To Unbox
+
+A Message To Unbox -  Stories by Prerna Sachdeva
+
+## Creator: Nishant Dogra
+http://dograsweblog.com/nishant-dogra/
+
---
+++ amessagetounbox/meta-tag-main-uncompressed.php
@@ -0,0 +1,199 @@
+<?php
+$url_cloud_path = "http://cloud.dograsweblog.com/";
+$url_assets_path = "";
+$url_cloud_path_assets = $url_cloud_path+$url_assets_path;
+$url_canonical = "http://amessagetounbox.com/";
+$url_domain_site = "http://amessagetounbox.com/";
+$url_domain_post = "http://amessagetounbox.com/";
+
+$title = "A Message To Unbox";
+$subtitle = "Stories by Prerna Sachdeva";
+$description = "A Message To Unbox - Stories by Prerna Sachdeva";
+$keywords = "articles, relationshiop, love, drama, affection, writing, prernasachdeva, message unbox";
+$article_tag = "articles, relationshiop, love, drama, affection, writing, prernasachdeva, message unbox";
+$site_name = "A Message To Unbox";
+$banner_image = $url_cloud_path+"amessagetounbox/cover-snippet.jpg";
+$creator = "@mrdogra007";
+$dograsweblog_hashtags = "DograsWeblog";
+
+$theme_color = "#885e0a";
+$publisher = "http://en.gravatar.com/prernasachdeva2202";
+$copyright = "Prerna Sachdeva";
+
+$see_also_twitter = "";
+$see_also_googleplus = "";
+$see_also_instagram = "";
+$see_also_facebook = "";
+$see_also_linkedin = "";
+$see_also_gravatar = "http://en.gravatar.com/prernasachdeva2202";
+
+$google_site_verification = "";
+$msvalidate_01 = "";
+$p_domain_verify = "";
+$yandex_verification = "";
+?>
+<meta charset="UTF-8">
+<title><?php echo $title ?></title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="x-dns-prefetch-control" content="on">
+<link rel="dns-prefetch" href="//cloud.dograsweblog.com" />
+<meta name="directory" content="submission">
+
+<meta name="description" content="<?php echo $description ?>">
+<meta name="keywords" content="<?php echo $keywords ?>">
+<meta name="subtitle" content="<?php echo $subtitle ?>">
+<link href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/32.png" rel="icon" type="image/x-icon" />
+	
+<!-- Article info tags -->
+<meta property="article:published_time" content="2018-04-25T13:10:00.000Z" />
+<meta property="article:modified_time" content="2018-05-24T22:12:48.000Z" />
+<meta property="article:publisher" content="<?php echo $generator ?>" />
+<meta property="article:author" content="<?php echo $see_also_googleplus ?>" />
+<meta property="article:section" content="Weblog" />
+<meta property="article:tag" content="<?php echo $article_tag ?>" />
+
+<!-- Facebook -->
+<meta property="og:title" content="<?php echo $title ?>" />
+<meta property="og:site_name" content="<?php echo $site_name ?>" />
+<meta property="og:description" content="<?php echo $description ?>" />
+<meta property="og:type" content="website" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="<?php echo $url_domain_post ?>" />
+<meta property="og:image" content="<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg" />
+<meta property="og:image:secure_url" content="<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg" />
+<meta property="og:image:width" content="750" />
+<meta property="og:image:height" content="410" />
+
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="<?php echo $title ?>" />
+<meta name="twitter:description" content="<?php echo $description ?>" />
+<meta name="twitter:url" content="<?php echo $url_domain_post ?>" />
+<meta name="twitter:image" content="<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg" />
+<meta name="twitter:image:src" content="<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg" />
+<meta name="twitter:site" content="@mrdogra007" />
+<meta name="twitter:creator" content="<?php echo $creator ?>" />
+<meta name="twitter:domain" content="<?php echo $url_domain_site ?>">
+
+<!-- Google Plus -->
+<meta itemprop="name" content="<?php echo $title ?>" />
+<link itemprop="url" href="<?php echo $url_domain_post ?>" />
+<meta itemprop="description" content="<?php echo $description ?>" />
+<meta itemprop="image" content="<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg" />
+
+<meta name="pinterest-rich-pin" content="true" />
+
+<!-- Dogras Weblog Hashtag -->
+<meta property="dograsweblog:hashtags" content="<?php echo $dograsweblog_hashtags ?>" />
+
+<!-- Mobile capable tags  -->
+<meta name="apple-mobile-web-app-title" content="<?php echo $title ?>">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name='apple-touch-fullscreen' content='yes'>
+<meta name="HandheldFriendly" content="True" />
+<meta name="MobileOptimized" content="320" />
+<meta name="mobile-web-app-capable" content="yes" />
+
+<!-- icons -->
+<link rel="shortcut icon" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/32.png" />
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/57.png" />
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/60.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/72.png" />
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/76.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/114.png" />
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/120.png" />
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/144.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/180.png" />
+
+<link rel="icon" type="image/png" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/160.png" sizes="160x160" />
+<link rel="icon" type="image/png" href="<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/196.png" sizes="196x196" />
+
+<meta name="msapplication-TileColor" content="<?php echo $theme_color ?>" />
+<meta name="msapplication-TileImage" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-144x144.png" />
+<meta name="msapplication-square70x70logo" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-70x70.png" />
+<meta name="msapplication-square144x144logo" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-144x144.png" />
+<meta name="msapplication-square150x150logo" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-150x150.png" />
+<meta name="msapplication-square310x310logo" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-310x310.png" />
+<meta name="msapplication-wide310x150logo" content ="<?php echo $url_cloud_path ?>amessagetounbox/favicons/mstile-310x150.png" />
+<meta name="msapplication-starturl" content="<?php echo $url_domain_site ?>" />
+<meta name="msapplication-tooltip" content="<?php echo $title ?>" />
+<meta name="msapplication-window" content="width=device-width;height=device-height" />
+<meta name="msapplication-task" content="name=<?php echo $title ?>;action-uri=<?php echo $url_cloud_path ?>amessagetounbox/logo-icons/32.png" />
+
+<meta name="theme-color" content="<?php echo $theme_color ?>">
+<meta name="application-name" content="<?php echo $title ?>" />
+<meta name="copyright" content="Nishant Dogra" />
+<meta name="publisher" content="<?php echo $see_also_googleplus ?>" />
+<meta name="generator" content="<?php echo $generator ?>" />
+<meta name="distribution" content="global" />
+<meta name="referrer" content="origin" />
+<link rel="canonical" href="<?php echo $url_canonical ?>" />
+
+<!-- Site verify -->
+<meta name="google-site-verification" content="<?php echo $google_site_verification ?>" />
+<meta name="msvalidate.01" content="<?php echo $msvalidate_01 ?>" />
+<meta name="p:domain_verify" content="<?php echo $p_domain_verify ?>"/>
+<meta name="yandex-verification" content="<?php echo $yandex_verification ?>" />
+
+<!-- Author social links -->
+<meta property="og:see_also" content="<?php echo $see_also_twitter ?>" />
+<meta property="og:see_also" content="<?php echo $see_also_googleplus ?>" />
+<meta property="og:see_also" content="<?php echo $see_also_instagram ?>" />
+<meta property="og:see_also" content="<?php echo $see_also_facebook ?>" />
+<meta property="og:see_also" content="<?php echo $see_also_linkedin ?>" />
+
+<link rel="manifest" href="<?php echo $url_cloud_path ?>amessagetounbox/manifest.json">
+	
+<script type='application/ld+json'>
+	{
+		"@context": "http://schema.org",
+		"@type": "Person",
+		"name": "<?php echo $copyright ?>",
+		"url": "<?php echo $url_canonical ?>",
+		"sameAs" : [
+		"<?php echo $see_also_twitter ?>",
+		"<?php echo $see_also_googleplus ?>",
+		"<?php echo $see_also_linkedin ?>",
+		"<?php echo $see_also_pinterest ?>"
+		"<?php echo $see_also_gravatar ?>"
+	] }
+</script>
+
+<script type='application/ld+json'>
+	{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "<?php echo $title ?>",
+		"url": "<?php echo $url_domain_site ?>",
+		"logo": "<?php echo $url_cloud_path ?>amessagetounbox/prerna-sachdeva.jpg",
+		"image": "<?php echo $url_cloud_path ?>amessagetounbox/cover-snippet.jpg",
+		"description": "<?php echo $title ?>",
+		"address": {
+			"@type": "PostalAddress",
+			"addressLocality": "Delhi",
+			"addressRegion": "ON",
+			"streetAddress": "Delhi",
+			"postalCode": "110001"
+		}
+	}
+</script>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-74432911-7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-74432911-7');
+</script>
+
+<!-- Heapan Alytics -->
+<script type="text/javascript">
+    window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
+      heap.load("98442744");
+</script>
